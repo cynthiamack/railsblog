89942ef3287331814f73fcdd161cb2122cddb867
cynthiamack <CYNTHIAMACK15@GMIAL.com>
2017-03-13 13:58:21 -0400

    tagging

---
+++ blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,13 @@
+class TagsController < ApplicationController
+	def show 
+		@tag = Tag.find(params[:id])
+	end
+	def index
+		@tag = Tag.all
+	end
+	def destroy 
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+		redirect_to action: 'index', status: 303
+	end
+end
